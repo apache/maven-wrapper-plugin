278a2aac1c347e961f086a49af5ddf3667b8a1e8
rfscholte <rfscholte@apache.org>
2020-05-23 13:27:01 +0200

    Add integration tests

--- src/main/java/org/apache/maven/plugins/wrapper/WrapperMojo.java
+++ src/main/java/org/apache/maven/plugins/wrapper/WrapperMojo.java
@@ -19,11 +19,13 @@ package org.apache.maven.plugins.wrapper;
  * under the License.
  */
 
+import java.io.BufferedWriter;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 import java.util.Properties;
 
@@ -42,6 +44,7 @@ import org.apache.maven.shared.transfer.artifact.DefaultArtifactCoordinate;
 import org.apache.maven.shared.transfer.artifact.resolve.ArtifactResolver;
 import org.apache.maven.shared.transfer.artifact.resolve.ArtifactResolverException;
 import org.codehaus.plexus.archiver.UnArchiver;
+import org.codehaus.plexus.components.io.fileselectors.FileSelector;
 
 /**
  * Adds the maven-wrapper files to this project 
@@ -49,44 +52,59 @@ import org.codehaus.plexus.archiver.UnArchiver;
  * @author Robert Scholte
  * @since 3.0.0
  */
-@Mojo( name = "wrapper", requiresProject = false, aggregator = true )
+@Mojo( name = "wrapper", aggregator = true, requiresDirectInvocation = true )
 public class WrapperMojo extends AbstractMojo
 {
     // CONFIGURATION PARAMETERS
     
     /**
-     * The version of Maven to require, default value is the Runtime version of Maven
+     * The version of Maven to require, default value is the Runtime version of Maven. 
+     * Can be any valid release above 2.0.9
      */
-    @Parameter
+    @Parameter( property = "mavenVersion" )
     private String mavenVersion;
-    
+
+    /**
+     * The version of the wrapper, default value is the Runtime version of Maven, should be at least 3.7.0
+     */
+    @Parameter( property = "wrapperVersion" )
+    private String wrapperVersion;
+
     /**
      * Options are:
      * <dl>
-     *   <dt>bin (default)</dt>
+     *   <dt>script (default)</dt>
+     *   <dd>only mvnw scripts</dd>
+     *   <dt>bin</dt>
      *   <dd>precompiled and packaged code</dd>
-     *   <dt>java</dt>
+     *   <dt>source</dt>
      *   <dd>sourcecode, will be compiled on the fly</dd>
      * </dl> 
      * 
      * Value will be used as classifier of the downloaded file
      */
-    @Parameter( defaultValue = "bin" )
+    @Parameter( defaultValue = "script", property = "type" )
     private String distributionType;
 
+    @Parameter( defaultValue = "true", property = "includeDebug" )
+    private boolean includeDebugScript;
+    
     // READONLY PARAMETERS 
     
     @Parameter( defaultValue = "${session}", readonly = true, required = true )
     private MavenSession session;
+    
+    // Waiting for org.codehaus.plexus.component.configurator.converters.basic.PathConverter
+    @Parameter( defaultValue = "${project.basedir}" )
+    private File basedir;
 
-    @Parameter( defaultValue = "org.apache.maven", readonly = true, required = true )
-    private String groupId;
+    // CONSTANTS
 
-    @Parameter( defaultValue = "maven-wrapper", readonly = true, required = true )
-    private String artifactId;
+    private String groupId = "org.apache.maven";
 
-    @Parameter( defaultValue = "tar.gz", readonly = true, required = true )
-    private String extension;
+    private String artifactId = "apache-maven-wrapper";
+
+    private String extension = "zip";
     
     // COMPONENTS 
     
@@ -109,18 +127,26 @@ public class WrapperMojo extends AbstractMojo
             throw new MojoExecutionException( e.getMessage(), e );
         }
         
-        Path targetFolder = Paths.get( ".mvn/wrapper" );
-
         try
         {
-            unpack( artifact, targetFolder );
+            unpack( artifact, basedir.toPath() );
         }
         catch ( IOException e )
         {
             throw new MojoExecutionException( e.getMessage(), e );
         }
         
-        update( targetFolder );
+        if ( mavenVersion != null )
+        {
+            try
+            {
+                replaceProperties( Files.createDirectories( basedir.toPath().resolve( ".mvn/wrapper" ) ) );
+            }
+            catch ( IOException e )
+            {
+                throw new MojoExecutionException( "can't create wrapper.proeprties", e );
+            }
+        }
     }
     
     private Artifact download()
@@ -132,7 +158,7 @@ public class WrapperMojo extends AbstractMojo
         DefaultArtifactCoordinate coordinate = new DefaultArtifactCoordinate();
         coordinate.setGroupId( groupId );
         coordinate.setArtifactId( artifactId );
-        coordinate.setVersion( getVersion() ); // 
+        coordinate.setVersion( getVersion( this.wrapperVersion ) ); 
         coordinate.setClassifier( distributionType );
         coordinate.setExtension( extension );
 
@@ -145,17 +171,55 @@ public class WrapperMojo extends AbstractMojo
         UnArchiver unarchiver = unarchivers.get( extension );
         unarchiver.setDestDirectory( targetFolder.toFile() );
         unarchiver.setSourceFile( artifact.getFile() );
+        if ( !includeDebugScript )
+        {
+            unarchiver.setFileSelectors( new FileSelector[] 
+                            {
+                                f -> !f.getName().contains( "Debug" ) 
+                            } );
+        }
         unarchiver.extract();
     }
     
-    private void update( Path targetFolder ) 
+    /**
+     * As long as the content only contains the license and the distributionUrl, we can simply replace it.
+     * No need to look for other properties, restore them, respecting comments, etc.
+     * 
+     * @param targetFolder the folder containing the wrapper.properties
+     * @throws IOException if writing fails
+     */
+    private void replaceProperties( Path targetFolder ) throws IOException
     {
-        // update wrapper.properties
+        Path wrapperPropertiesFile = targetFolder.resolve( "maven-wrapper.properties" );
+        
+        final String license = "# Licensed to the Apache Software Foundation (ASF) under one\n"
+            + "# or more contributor license agreements.  See the NOTICE file\n"
+            + "# distributed with this work for additional information\n"
+            + "# regarding copyright ownership.  The ASF licenses this file\n"
+            + "# to you under the Apache License, Version 2.0 (the\n"
+            + "# \"License\"); you may not use this file except in compliance\n"
+            + "# with the License.  You may obtain a copy of the License at\n" + "# \n"
+            + "#   http://www.apache.org/licenses/LICENSE-2.0\n" + "# \n"
+            + "# Unless required by applicable law or agreed to in writing,\n"
+            + "# software distributed under the License is distributed on an\n"
+            + "# \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY\n"
+            + "# KIND, either express or implied.  See the License for the\n"
+            + "# specific language governing permissions and limitations\n" + "# under the License.\n";
+        
+        try ( BufferedWriter out = Files.newBufferedWriter( wrapperPropertiesFile ) )
+        {
+            out.append( license );
+            out.append( "distributionUrl=https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/"
+                + mavenVersion
+                + "/apache-maven-"
+                + mavenVersion
+                + "-bin.zip" );
+        }
     }
     
-    private String getVersion()
+    private String getVersion( String defaultVersion )
     {
-        String version = this.mavenVersion;
+        String version = defaultVersion;
         if ( version == null )
         {
             Properties props = new Properties();
