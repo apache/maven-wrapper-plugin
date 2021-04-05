a8adf99ba4f975ef62a91435c3295f5478a4d5b7
rfscholte <rfscholte@apache.org>
2021-04-05 14:38:56 +0200

    [MWRAPPER-8] Let plugin fail on Maven 3

--- src/main/java/org/apache/maven/plugins/wrapper/WrapperMojo.java
+++ src/main/java/org/apache/maven/plugins/wrapper/WrapperMojo.java
@@ -36,6 +36,7 @@ import org.apache.maven.artifact.Artifact;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.DefaultProjectBuildingRequest;
@@ -65,7 +66,7 @@ public class WrapperMojo extends AbstractMojo
     private String mavenVersion;
 
     /**
-     * The version of the wrapper, default value is the Runtime version of Maven, should be at least 3.7.0
+     * The version of the wrapper, default value is the Runtime version of Maven, should be at least 4
      */
     @Parameter( property = "wrapperVersion" )
     private String wrapperVersion;
@@ -115,12 +116,19 @@ public class WrapperMojo extends AbstractMojo
     private Map<String, UnArchiver> unarchivers;
     
     @Override
-    public void execute() throws MojoExecutionException
+    public void execute() throws MojoExecutionException, MojoFailureException
     {
+        String wrapperVersion = getVersion( this.wrapperVersion );
+        if ( wrapperVersion.startsWith( "3." ) )
+        {
+            throw new MojoFailureException( "wrapperVersion not supported for Maven " + wrapperVersion + ","
+                + " it must be at least 4." );
+        }
+        
         Artifact artifact;
         try
         {
-            artifact = download();
+            artifact = downloadWrapper( wrapperVersion );
         }
         catch ( ArtifactResolverException e )
         {
@@ -150,7 +158,7 @@ public class WrapperMojo extends AbstractMojo
         }
     }
     
-    private Artifact download()
+    private Artifact downloadWrapper( String wrapperVersion )
         throws ArtifactResolverException
     {
         ProjectBuildingRequest buildingRequest =
@@ -159,7 +167,7 @@ public class WrapperMojo extends AbstractMojo
         DefaultArtifactCoordinate coordinate = new DefaultArtifactCoordinate();
         coordinate.setGroupId( WRAPPER_GROUP_ID );
         coordinate.setArtifactId( WRAPPER_ARTIFACT_ID );
-        coordinate.setVersion( getVersion( this.wrapperVersion ) ); 
+        coordinate.setVersion( wrapperVersion ); 
         coordinate.setClassifier( distributionType );
         coordinate.setExtension( WRAPPER_EXTENSION );
 
